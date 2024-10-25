2e56b632972ff6d10f2912c4b257b15cca5d9a91
farelrunin <fhariel28@gmail.com>
2024-10-25 08:08:02 +0700

    login

--- lib/main.dart
+++ lib/main.dart
@@ -1,25 +1,34 @@
 import 'package:flutter/material.dart';
-import 'pages/home_page.dart';
-import 'pages/detail_page.dart';
-import 'pages/cart_page.dart';
-import 'pages/about_page.dart';
+import 'config/routes/routes.dart';
+import 'screens/login_screen.dart';
 
 void main() {
-  runApp(MyApp());
+  runApp(const MyApp());
 }
 
 class MyApp extends StatelessWidget {
+  const MyApp({Key? key}) : super(key: key);
+
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      title: 'Toko Roti',
-      initialRoute: '/',
-      routes: {
-        '/': (context) => HomePage(),
-        '/detail': (context) => DetailPage(),
-        '/keranjang': (context) => CartPage(),
-        '/about': (context) => AboutPage(),
-      },
+      debugShowCheckedModeBanner: false,
+      title: 'Login UI',
+      theme: ThemeData(
+        primarySwatch: Colors.blue,
+        elevatedButtonTheme: ElevatedButtonThemeData(
+          style: ElevatedButton.styleFrom(
+            backgroundColor: const Color(0xFF4E7D96),
+            foregroundColor: Colors.white,
+            padding: const EdgeInsets.symmetric(horizontal: 40, vertical: 15),
+            shape: RoundedRectangleBorder(
+              borderRadius: BorderRadius.circular(30),
+            ),
+          ),
+        ),
+      ),
+      initialRoute: '/login',
+      routes: AppRoutes.routes,
     );
   }
 }
